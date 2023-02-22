e66b3c59595d37bbaa5e92b781a767e1089a470c
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-02-22 10:38:34 +0900

    [Test] initial test

---
+++ tests/integration_test.rs
@@ -0,0 +1,22 @@
+extern crate vachelin;
+
+use axum::{
+    body::Body,
+    http::{Request, StatusCode},
+};
+use tower::ServiceExt;
+use vachelin::app;
+
+#[tokio::test]
+async fn hello_world() {
+    let app = app().await;
+
+    let response = app
+        .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+        .await
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+    assert_eq!(&body[..], b"Hello, Rust!");
+}
