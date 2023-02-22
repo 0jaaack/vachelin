8a74e2da78fc970265684da5b45fa7da0e8e876e
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-02-22 10:38:14 +0900

    [Setting] library crate setting

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use axum::{routing::get, Router};
+use dotenv::dotenv;
+use mongodb::{options::ClientOptions, Client};
+use std::env;
+
+pub async fn app() -> Router {
+    let client = client().await;
+
+    Router::new()
+        .route("/", get(|| async { "Hello, Rust!" }))
+        .with_state(client)
+}
+
+async fn client() -> Client {
+    dotenv().ok();
+
+    let mongo_uri: String =
+        env::var("MONGO_URI").expect("Failed to load `MONGO_URI` environment variable.");
+
+    let mut client_options = ClientOptions::parse(mongo_uri).await.unwrap();
+    client_options.app_name = Some("vachelin".to_string());
+
+    Client::with_options(client_options).unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
-use axum::{routing::get, Router};
-use dotenv::dotenv;
-use mongodb::{options::ClientOptions, Client};
+extern crate vachelin;
+
+use axum::Router;
 
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
-
-    let mongo_uri: String =
-        std::env::var("MONGO_URI").expect("Failed to load `MONGO_URI` environment variable.");
-
-    let mut client_options = ClientOptions::parse(mongo_uri).await.unwrap();
-    client_options.app_name = Some("mytimes".to_string());
-    let client = Client::with_options(client_options).unwrap();
-
-    let app = Router::new().route("/", get(|| async { "Hello, Rust!" }));
-    let app = app.with_state(client);
-
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
+        .serve(app().await.into_make_service())
         .await
         .unwrap();
+}
+
+pub async fn app() -> Router {
+    vachelin::app().await
 }
